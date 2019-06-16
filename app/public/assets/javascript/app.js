45e330c264de0649d5feeb3aa5330a5059f27458
Jenny Ivy <47339542+jenivy06@users.noreply.github.com>
2019-06-15 19:35:27 -0600

    finished jquery to take in user input in survey.html

--- app/public/assets/javascript/app.js
+++ app/public/assets/javascript/app.js
@@ -1,7 +1,9 @@
 $(document).ready(function() { 
 var userScoreQ1 = 0
+var answered = 0
 $("#q1b1").click(function(){
     userScoreQ1++
+    answered++
     console.log("User score Q1: " + userScoreQ1);
     $("#q1b2").hide();
     $("#q1b3").hide();
@@ -11,6 +13,7 @@ $("#q1b1").click(function(){
 
   $("#q1b2").click(function(){
     userScoreQ1+=2
+    answered++
     console.log("User score Q1: " + userScoreQ1);
     $("#q1b1").hide();
     $("#q1b3").hide();
@@ -20,6 +23,7 @@ $("#q1b1").click(function(){
 
   $("#q1b3").click(function(){
     userScoreQ1+=3
+    answered++
     console.log("User score Q1: " + userScoreQ1);
     $("#q1b1").hide();
     $("#q1b2").hide();
@@ -29,6 +33,7 @@ $("#q1b1").click(function(){
 
   $("#q1b4").click(function(){
     userScoreQ1+=4
+    answered++
     console.log("User score Q1: " + userScoreQ1);
     $("#q1b1").hide();
     $("#q1b2").hide();
@@ -38,6 +43,7 @@ $("#q1b1").click(function(){
 
   $("#q1b5").click(function(){
     userScoreQ1+=5
+    answered++
     console.log("User score Q1: " + userScoreQ1);
     $("#q1b1").hide();
     $("#q1b2").hide();
@@ -48,6 +54,7 @@ $("#q1b1").click(function(){
   var userScoreQ2 = 0
   $("#q2b1").click(function(){
     userScoreQ2++
+    answered++
     console.log("User score Q2: " + userScoreQ2);
     $("#q2b2").hide();
     $("#q2b3").hide();
@@ -58,6 +65,7 @@ $("#q1b1").click(function(){
   var userScoreQ2 = 0
   $("#q2b2").click(function(){
     userScoreQ2+=2
+    answered++
     console.log("User score Q2: " + userScoreQ2);
     $("#q2b1").hide();
     $("#q2b3").hide();
@@ -68,6 +76,7 @@ $("#q1b1").click(function(){
   var userScoreQ2 = 0
   $("#q2b3").click(function(){
     userScoreQ2+=3
+    answered++
     console.log("User score Q2: " + userScoreQ2);
     $("#q2b1").hide();
     $("#q2b2").hide();
@@ -78,6 +87,7 @@ $("#q1b1").click(function(){
   var userScoreQ2 = 0
   $("#q2b4").click(function(){
     userScoreQ2+=4
+    answered++
     console.log("User score Q2: " + userScoreQ2);
     $("#q2b1").hide();
     $("#q2b3").hide();
@@ -88,6 +98,7 @@ $("#q1b1").click(function(){
   var userScoreQ2 = 0
   $("#q2b5").click(function(){
     userScoreQ2+=5
+    answered++
     console.log("User score Q2: " + userScoreQ2);
     $("#q2b1").hide();
     $("#q2b3").hide();
@@ -98,6 +109,7 @@ $("#q1b1").click(function(){
   var userScoreQ3 = 0
   $("#q3b1").click(function(){
     userScoreQ3++
+    answered++
     console.log("User score Q3: " + userScoreQ3);
     $("#q3b1").hide();
     $("#q3b3").hide();
@@ -108,6 +120,7 @@ $("#q1b1").click(function(){
   var userScoreQ3 = 0
   $("#q3b2").click(function(){
     userScoreQ3+=2
+    answered++
     console.log("User score Q3: " + userScoreQ3);
     $("#q3b1").hide();
     $("#q3b3").hide();
@@ -118,6 +131,7 @@ $("#q1b1").click(function(){
   var userScoreQ3 = 0
   $("#q3b3").click(function(){
     userScoreQ3+=3
+    answered++
     console.log("User score Q3: " + userScoreQ3);
     $("#q3b1").hide();
     $("#q3b2").hide();
@@ -128,6 +142,7 @@ $("#q1b1").click(function(){
   var userScoreQ3 = 0
   $("#q3b4").click(function(){
     userScoreQ3+=4
+    answered++
     console.log("User score Q3: " + userScoreQ3);
     $("#q3b1").hide();
     $("#q3b2").hide();
@@ -138,6 +153,7 @@ $("#q1b1").click(function(){
   var userScoreQ3 = 0
   $("#q3b5").click(function(){
     userScoreQ3+=5
+    answered++
     console.log("User score Q3: " + userScoreQ3);
     $("#q3b1").hide();
     $("#q3b2").hide();
@@ -148,6 +164,7 @@ $("#q1b1").click(function(){
   var userScoreQ4 = 0
   $("#q4b1").click(function(){
     userScoreQ4++
+    answered++
     console.log("User score Q4: " + userScoreQ4);
     $("#q4b5").hide();
     $("#q4b3").hide();
@@ -158,8 +175,8 @@ $("#q1b1").click(function(){
   var userScoreQ4 = 0
   $("#q4b2").click(function(){
     userScoreQ4+=2
+    answered++
     console.log("User score Q4: " + userScoreQ4);
-    $("#q4b1").hide();
     $("#q4b3").hide();
     $("#q4b4").hide();
     $("#q4b5").hide();
@@ -168,6 +185,7 @@ $("#q1b1").click(function(){
   var userScoreQ4 = 0
   $("#q4b3").click(function(){
     userScoreQ4+=3
+    answered++
     console.log("User score Q4: " + userScoreQ4);
     $("#q4b1").hide();
     $("#q4b2").hide();
@@ -178,6 +196,7 @@ $("#q1b1").click(function(){
   var userScoreQ4 = 0
   $("#q4b4").click(function(){
     userScoreQ4+=4
+    answered++
     console.log("User score Q4: " + userScoreQ4);
     $("#q4b1").hide();
     $("#q4b2").hide();
@@ -188,6 +207,7 @@ $("#q1b1").click(function(){
   var userScoreQ4 = 0
   $("#q4b5").click(function(){
     userScoreQ4+=5
+    answered++
     console.log("User score Q4: " + userScoreQ4);
     $("#q4b1").hide();
     $("#q4b2").hide();
@@ -198,6 +218,7 @@ $("#q1b1").click(function(){
   var userScoreQ5 = 0
   $("#q5b1").click(function(){
     userScoreQ5++
+    answered++
     console.log("User score Q5: " + userScoreQ5);
     $("#q5b5").hide();
     $("#q5b3").hide();
@@ -208,6 +229,7 @@ $("#q1b1").click(function(){
   var userScoreQ5 = 0
   $("#q5b2").click(function(){
     userScoreQ5+=2
+    answered++
     console.log("User score Q5: " + userScoreQ5);
     $("#q5b5").hide();
     $("#q5b3").hide();
@@ -218,6 +240,7 @@ $("#q1b1").click(function(){
   var userScoreQ5 = 0
   $("#q5b3").click(function(){
     userScoreQ5+=3
+    answered++
     console.log("User score Q5: " + userScoreQ5);
     $("#q5b5").hide();
     $("#q5b2").hide();
@@ -228,6 +251,7 @@ $("#q1b1").click(function(){
   var userScoreQ5 = 0
   $("#q5b4").click(function(){
     userScoreQ5+=4
+    answered++
     console.log("User score Q5: " + userScoreQ5);
     $("#q5b5").hide();
     $("#q5b2").hide();
@@ -238,6 +262,7 @@ $("#q1b1").click(function(){
   var userScoreQ5 = 0
   $("#q5b5").click(function(){
     userScoreQ5+=5
+    answered++
     console.log("User score Q5: " + userScoreQ5);
     $("#q5b3").hide();
     $("#q5b2").hide();
@@ -248,6 +273,7 @@ $("#q1b1").click(function(){
   var userScoreQ6 = 0
   $("#q6b1").click(function(){
     userScoreQ6++
+    answered++
     console.log("User score Q6: " + userScoreQ6);
     $("#q6b5").hide();
     $("#q6b3").hide();
@@ -258,6 +284,7 @@ $("#q1b1").click(function(){
   var userScoreQ6 = 0
   $("#q6b2").click(function(){
     userScoreQ6+=2
+    answered++
     console.log("User score Q6: " + userScoreQ6);
     $("#q6b5").hide();
     $("#q6b3").hide();
@@ -268,6 +295,7 @@ $("#q1b1").click(function(){
   var userScoreQ6 = 0
   $("#q6b3").click(function(){
     userScoreQ6+=3
+    answered++
     console.log("User score Q6: " + userScoreQ6);
     $("#q6b5").hide();
     $("#q6b2").hide();
@@ -278,6 +306,7 @@ $("#q1b1").click(function(){
   var userScoreQ6 = 0
   $("#q6b4").click(function(){
     userScoreQ6+=4
+    answered++
     console.log("User score Q6: " + userScoreQ6);
     $("#q6b5").hide();
     $("#q6b2").hide();
@@ -288,6 +317,7 @@ $("#q1b1").click(function(){
   var userScoreQ6 = 0
   $("#q6b5").click(function(){
     userScoreQ6+=5
+    answered++
     console.log("User score Q6: " + userScoreQ6);
     $("#q6b4").hide();
     $("#q6b2").hide();
@@ -298,6 +328,7 @@ $("#q1b1").click(function(){
   var userScoreQ7 = 0
   $("#q7b1").click(function(){
     userScoreQ7++
+    answered++
     console.log("User score Q7: " + userScoreQ7);
     $("#q7b5").hide();
     $("#q7b3").hide();
@@ -308,6 +339,7 @@ $("#q1b1").click(function(){
   var userScoreQ7 = 0
   $("#q7b2").click(function(){
     userScoreQ7+=2
+    answered++
     console.log("User score Q7: " + userScoreQ7);
     $("#q7b5").hide();
     $("#q7b3").hide();
@@ -318,6 +350,7 @@ $("#q1b1").click(function(){
   var userScoreQ7 = 0
   $("#q7b3").click(function(){
     userScoreQ7+=3
+    answered++
     console.log("User score Q7: " + userScoreQ7);
     $("#q7b5").hide();
     $("#q7b2").hide();
@@ -328,6 +361,7 @@ $("#q1b1").click(function(){
   var userScoreQ7 = 0
   $("#q7b4").click(function(){
     userScoreQ7+=4
+    answered++
     console.log("User score Q7: " + userScoreQ7);
     $("#q7b5").hide();
     $("#q7b2").hide();
@@ -338,6 +372,7 @@ $("#q1b1").click(function(){
   var userScoreQ7 = 0
   $("#q7b5").click(function(){
     userScoreQ7+=5
+    answered++
     console.log("User score Q7: " + userScoreQ7);
     $("#q7b4").hide();
     $("#q7b2").hide();
@@ -348,6 +383,7 @@ $("#q1b1").click(function(){
   var userScoreQ8 = 0
   $("#q8b1").click(function(){
     userScoreQ8++
+    answered++
     console.log("User score Q8: " + userScoreQ8);
     $("#q8b5").hide();
     $("#q8b3").hide();
@@ -358,6 +394,7 @@ $("#q1b1").click(function(){
   var userScoreQ8 = 0
   $("#q8b2").click(function(){
     userScoreQ8+=2
+    answered++
     console.log("User score Q8: " + userScoreQ8);
     $("#q8b5").hide();
     $("#q8b3").hide();
@@ -368,6 +405,7 @@ $("#q1b1").click(function(){
   var userScoreQ8 = 0
   $("#q8b3").click(function(){
     userScoreQ8+=3
+    answered++
     console.log("User score Q8: " + userScoreQ8);
     $("#q8b5").hide();
     $("#q8b2").hide();
@@ -378,6 +416,7 @@ $("#q1b1").click(function(){
   var userScoreQ8 = 0
   $("#q8b4").click(function(){
     userScoreQ8+=4
+    answered++
     console.log("User score Q8: " + userScoreQ8);
     $("#q8b5").hide();
     $("#q8b2").hide();
@@ -388,6 +427,7 @@ $("#q1b1").click(function(){
   var userScoreQ8 = 0
   $("#q8b5").click(function(){
     userScoreQ8+=5
+    answered++
     console.log("User score Q8: " + userScoreQ8);
     $("#q8b4").hide();
     $("#q8b2").hide();
@@ -398,6 +438,7 @@ $("#q1b1").click(function(){
   var userScoreQ9 = 0
   $("#q9b1").click(function(){
     userScoreQ9++
+    answered++
     console.log("User score Q9: " + userScoreQ9);
     $("#q9b5").hide();
     $("#q9b3").hide();
@@ -408,6 +449,7 @@ $("#q1b1").click(function(){
   var userScoreQ9 = 0
   $("#q9b2").click(function(){
     userScoreQ9+=2
+    answered++
     console.log("User score Q9: " + userScoreQ9);
     $("#q9b5").hide();
     $("#q9b3").hide();
@@ -418,6 +460,7 @@ $("#q1b1").click(function(){
   var userScoreQ9 = 0
   $("#q9b3").click(function(){
     userScoreQ9+=3
+    answered++
     console.log("User score Q9: " + userScoreQ9);
     $("#q9b5").hide();
     $("#q9b2").hide();
@@ -428,6 +471,7 @@ $("#q1b1").click(function(){
   var userScoreQ9 = 0
   $("#q9b4").click(function(){
     userScoreQ9+=4
+    answered++
     console.log("User score Q9: " + userScoreQ9);
     $("#q9b5").hide();
     $("#q9b2").hide();
@@ -438,6 +482,7 @@ $("#q1b1").click(function(){
   var userScoreQ9 = 0
   $("#q9b5").click(function(){
     userScoreQ9+=5
+    answered++
     console.log("User score Q9: " + userScoreQ9);
     $("#q9b4").hide();
     $("#q9b2").hide();
@@ -448,6 +493,7 @@ $("#q1b1").click(function(){
   var userScoreQ10 = 0
   $("#q10b1").click(function(){
     userScoreQ10++
+    answered++
     console.log("User score Q10: " + userScoreQ10);
     $("#q10b5").hide();
     $("#q10b3").hide();
@@ -458,6 +504,7 @@ $("#q1b1").click(function(){
   var userScoreQ10 = 0
   $("#q10b2").click(function(){
     userScoreQ10+=2
+    answered++
     console.log("User score Q10: " + userScoreQ10);
     $("#q10b5").hide();
     $("#q10b3").hide();
@@ -468,6 +515,7 @@ $("#q1b1").click(function(){
   var userScoreQ10 = 0
   $("#q10b3").click(function(){
     userScoreQ10+=3
+    answered++
     console.log("User score Q10: " + userScoreQ10);
     $("#q10b5").hide();
     $("#q10b2").hide();
@@ -478,6 +526,7 @@ $("#q1b1").click(function(){
   var userScoreQ10 = 0
   $("#q10b4").click(function(){
     userScoreQ10+=4
+    answered++
     console.log("User score Q10: " + userScoreQ10);
     $("#q10b5").hide();
     $("#q10b2").hide();
@@ -488,6 +537,7 @@ $("#q1b1").click(function(){
   var userScoreQ10 = 0
   $("#q10b5").click(function(){
     userScoreQ10+=5
+    answered++
     console.log("User score Q10: " + userScoreQ10);
     $("#q10b4").hide();
     $("#q10b2").hide();
@@ -495,6 +545,17 @@ $("#q1b1").click(function(){
     $("#q10b1").hide();
   });
 
+  $("#submitbtn").click(function(){
+    event.preventDefault();
+     // Validate all that all questions have been answered
+if (answered <= 9)    
+alert("You forgot to answer one or more questions!");
+
+else if (answered === 10) {
+var totalUserScore = [userScoreQ1, userScoreQ2, userScoreQ3, userScoreQ4, userScoreQ5, userScoreQ6, userScoreQ7, userScoreQ8, userScoreQ9, userScoreQ10];
+console.log(totalUserScore);
+}
+});
 })
 
 
